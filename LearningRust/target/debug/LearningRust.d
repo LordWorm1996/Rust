a0b43c9814f45dfd5dbdee2525c5bf214570fa09
Dimitrios Psaroudakis <73707939+LordWorm1996@users.noreply.github.com>
2024-12-05 20:10:49 +0100

    Delete LearningRust directory

--- LearningRust/target/debug/LearningRust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\dimit\Desktop\Dimitrios\Programming\Rust\LearningRust\target\debug\LearningRust.exe: C:\Users\dimit\Desktop\Dimitrios\Programming\Rust\LearningRust\src\main.rs
--- LearningRust/target/debug/deps/LearningRust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dimit\Desktop\Dimitrios\Programming\Rust\LearningRust\target\debug\deps\LearningRust.exe: src/main.rs
-
-C:\Users\dimit\Desktop\Dimitrios\Programming\Rust\LearningRust\target\debug\deps\LearningRust.d: src/main.rs
-
-src/main.rs:
